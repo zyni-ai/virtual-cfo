d4d8f819da98bba8d4849d89a2df52ce2eec473e
Varun U <varun@zysk.tech>
2026-02-25 15:21:31 +0530

    feat(seeds): add default Indian chart of accounts seeder (#21) (#28)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,10 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->call([
+            AccountHeadSeeder::class,
+        ]);
+
         if (app()->environment('local', 'testing')) {
             User::factory()->create([
                 'name' => 'Admin',
